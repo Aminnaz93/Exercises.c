ee50e6c7f6b46bf2a13b215942de943cd374b863
aminnaz93 <Aminnazari040@hotmail.com>
2024-07-15 18:07:23 +0200

    gvfds

--- main.c
+++ main.c
@@ -28,15 +28,43 @@ printf("The result is: %d\n", sum);*/
 //#####################################
 
 
-int num = 20;
+/*int num = 20;
 float MyFloat = 2.32;
 double MyDouble = 2.323423;
 char MyChar = 'C';
 
 printf("%d\n",num);
-printf("%f\n", MyFloat);
-printf("%f\n", MyDouble);
-printf("%c\n", MyChar);
+printf("%.2f\n", MyFloat);
+printf("%.2f\n", MyDouble);
+printf("%c\n", MyChar);*/
+
+//#####################################
+
+
+/*int length = 20;
+int height = 10;
+int area = length * height;
+double num = 2.33234233;
+
+printf("The length is: %d\n", length);
+printf("The height is: %d\n", height);
+printf("The area is: %d\n", area);*/
+
+
+//####################################
+
+int num = 20;
+float myfloat = 20.2;
+double mydouble = 20.3234;
+char myChar = 'A';
+
+
+printf("%lu\n", sizeof(num));
+printf("%lu\n", sizeof(myfloat));
+printf("%lu\n", sizeof(mydouble));
+printf("%lu\n", sizeof(myChar));
+
+
 
 
 return 0;   

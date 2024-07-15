308823f7573fb4d80b272f25c9a8d3a01277be1d
aminnaz93 <Aminnazari040@hotmail.com>
2024-07-15 16:44:31 +0200

    da

---
+++ Exercises.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int add(int a, int b){
+    return a + b;
+}
+
+int main() {
+
+
+printf("Hello\n");
+
+int sum = add(2,3);
+
+printf("summan är: %d\n" , sum);
+
+
+return 0;   
+}
